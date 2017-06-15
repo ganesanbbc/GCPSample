a0ec5a4d24e1e5ada60d065dd107baf4a8b3b2ba
Ganesan Natarajan Shanmugam <ganesan.ns@cognizant.com>
2017-06-15 13:57:46 +0530

    implement google data store

--- src/main/java/com/cts/ms/wo/dao/WorkOrderDAO.java
+++ src/main/java/com/cts/ms/wo/dao/WorkOrderDAO.java
@@ -1,11 +1,36 @@
 package com.cts.ms.wo.dao;
 
 import com.cts.ms.wo.vo.WorkOrder;
+import com.google.cloud.datastore.Datastore;
+import com.google.cloud.datastore.DatastoreOptions;
+import com.google.cloud.datastore.Entity;
+import com.google.cloud.datastore.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 
 @Repository
 public class WorkOrderDAO {
+
+    public static final String TABLE_NAME = "Task";
+
     public WorkOrder getOrders() {
-        return new WorkOrder("demo");
+         List<WorkOrder> workOrderList = new ArrayList();
+
+        Datastore datastore = DatastoreOptions.getDefaultInstance().getService();
+        Query<Entity> query =
+                Query.newEntityQueryBuilder().setKind(TABLE_NAME).build();
+
+        Iterator<Entity> result = datastore.run(query);
+
+        while (result.hasNext()) {
+            Entity task = result.next();
+            String name = task.getString("name");
+            workOrderList.add(new WorkOrder(name));
+        }
+
+        return workOrderList.get(0);
     }
 }
