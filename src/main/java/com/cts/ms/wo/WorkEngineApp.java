f6e0062ab28439769dddae09b5a81b1ee9692db1
Navaneethan <navaneethan.t@cognizant.com>
2017-07-04 10:40:36 +0530

    Added cross origin

--- src/main/java/com/cts/ms/wo/WorkEngineApp.java
+++ src/main/java/com/cts/ms/wo/WorkEngineApp.java
@@ -2,11 +2,28 @@ package com.cts.ms.wo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @SpringBootApplication
 public class WorkEngineApp {
+	
+	String ORGIN_URL = "https://customermssample.appspot.com";
 
 	public static void main(String[] args) {
 		SpringApplication.run(WorkEngineApp.class, args);
 	}
+	
+	
+	@Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurerAdapter() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping(ServiceEndPoint.POST_ORDERS).allowedOrigins(ORGIN_URL);
+            }
+        };
+    }
 }
